00ad1ac7941da171581b6d86470a6bfa39592df4
Debleena18 <sarkardebleena18@gmail.com>
2022-04-08 13:26:41 +0530

    CSS Module Styling

--- src/components/CourseGoals/CourseInput/CourseInput.js
+++ src/components/CourseGoals/CourseInput/CourseInput.js
@@ -1,33 +1,7 @@
 import React, { useState } from 'react';
-import styled from 'styled-components';
-import Button from '../../UI/Button/Button';
-import './CourseInput.css';
 
-//Here "&" is use as a pseudo selectors when we want to target nexted elements under Div func such as here: label,input,etc
-//This syntax is used when we use Styled Components.
-const FormControl = styled.div`
-  margin: 0.5rem 0;
-  & label {
-    font-weight: bold;
-    display: block;
-    margin-bottom: 0.5rem;
-    color: ${props => (props.invalid ? 'red' : 'black')};
-  }
-  & input {
-    display: block;
-    width: 100%;
-    border: 1px solid ${props => (props.invalid ? 'red' : '#ccc')};
-    background: ${props => (props.invalid ? '#ffd7d7' : 'transparent')};
-    font: inherit;
-    line-height: 1.5rem;
-    padding: 0 0.25rem;
-  }
-  & input:focus {
-    outline: none;
-    background: #fad0ec;
-    border-color: #8b005d;
-  }
-`;
+import Button from '../../UI/Button/Button';
+import styles from './CourseInput.module.css';
 
 const CourseInput = props => {
   const [enteredValue, setEnteredValue] = useState('');
@@ -48,33 +22,18 @@ const CourseInput = props => {
     }
     props.onAddGoal(enteredValue);
   };
-//This is Dynamic Styling changes have been made in css file ie. invalid input, invalid label
+
   return (
     <form onSubmit={formSubmitHandler}>
-      <FormControl invalid={!isValid}>
-      {/* <FormControl className={!isValid && 'invalid'}> */}
+      <div
+        className={`${styles['form-control']} ${!isValid && styles.invalid}`}
+      >
         <label>Course Goal</label>
         <input type="text" onChange={goalInputChangeHandler} />
-      </FormControl>
+      </div>
       <Button type="submit">Add Goal</Button>
     </form>
   );
 };
-
-//Below is the InLine Styling alternative 
-  // return (
-  //   <form onSubmit={formSubmitHandler}>
-  //     <div className="form-control">
-  //       <label style={{color: !isValid ? 'red' : 'black'}}>Course Goal</label>
-  //       <input style={{
-  //         borderColor: !isValid ? 'red' : 'black',
-  //         background: !isValid ? 'salmon' : 'transparent'
-  //         }}
-  //         type="text" onChange={goalInputChangeHandler} />
-  //     </div>
-  //     <Button type="submit">Add Goal</Button>
-  //   </form>
-  // );
-//};
 
 export default CourseInput;
